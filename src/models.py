0af4f437d9e1fe0cc10874c128ee2edc1818888d
Hossein Keshavarz <hosseinkeshavarz1997@gmail.com>
2021-03-24 16:07:42 -0400

    change code design

--- src/models.py
+++ src/models.py
@@ -34,6 +34,7 @@ import os
 import pandas as pd
 import numpy as np
 from pydriller import GitRepository
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.utils import shuffle
 
@@ -43,76 +44,85 @@ from gensim.summarization import bm25
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 data_path = os.path.join(BASE_DIR, 'data')
 
-train_df = get_data(mode='train')
-with open(os.path.join(data_path, 'clean_added.json')) as file:
-    added = json.load(file)
-with open(os.path.join(data_path, 'clean_deleted.json')) as file:
-    deleted = json.load(file)
 
-hashes_train = list(pd.concat([train_df['HashId'], train_df['FixHashId']]).unique())
-corpus = list()
-for hash in hashes_train:
-    corpus += added.get(hash, {}).values()
-    corpus += deleted.get(hash, {}).values()
-tfidf_model = feature_extractor(corpus)
+class ActiveSZZ:
+    def __init__(self, project_path):
+        self.train_df = get_data(mode='train')
+        with open(os.path.join(data_path, 'clean_added.json')) as file:
+            self.added = json.load(file)
+        with open(os.path.join(data_path, 'clean_deleted.json')) as file:
+            self.deleted = json.load(file)
+        self.git_repo = GitRepository(project_path)
+        self.hashes_train = list(pd.concat([self.train_df['HashId'], self.train_df['FixHashId']]).unique())
+        self.clf = LogisticRegression(random_state=0)
+        self.bm25 = None
+        self.tfidf_model = None
+        self.add_corpus = list()
+        self.inv_added = list()
 
-# for now: search among all, everything! no matter when or what file it is about.
-add_corpus = list()
-inv_added = list()
-for hash in hashes_train:
-    for file, doc in added.get(hash, {}).items():
-        add_corpus.append(doc)
-        inv_added.append(str(file + ' @ ' + hash))
+    def extract_features(self):
+        corpus = list()
+        for hash in self.hashes_train:
+            corpus += self.added.get(hash, {}).values()
+            corpus += self.deleted.get(hash, {}).values()
+        self.tfidf_model = TfidfVectorizer(tokenizer=lambda doc: doc, lowercase=False)
+        self.tfidf_model.fit(corpus)
 
-del added
-del corpus
+    # for now: search among all, everything! no matter when or what file it is about.
+    def index_docs(self):
+        for hash in self.hashes_train:
+            for file, doc in self.added.get(hash, {}).items():
+                self.add_corpus.append(doc)
+                self.inv_added.append(str(file + ' @ ' + hash))
+        self.bm25 = bm25.BM25(self.add_corpus)
+        del self.added
+
+    def check_szz(self, hash, file, retrieved):
+        commit = self.git_repo.get_commit(hash)
+        last_modified = list(self.git_repo.get_commits_last_modified_lines(commit)[file])
+        labels = [int(h in last_modified) for h in retrieved]
+        return labels
+
+    def train(self):
+        for i, row in self.train_df.iterrows():
+            fix_hash = row['FixHashId']
+            file = row['File']
+            query = self.deleted[fix_hash][file]
+            scores = self.bm25.get_scores(query)
+            best_idx = sorted(range(len(scores)), key=lambda j: scores[j], reverse=True)[0]
+
+            data = dict()
+            data[best_idx] = 1
+            labeled_indices = [best_idx]
+            batch_size = 1
+            while True:
+                indices = np.delete(np.arange(len(self.add_corpus)), labeled_indices)
+                np.random.shuffle(indices)
+                for idx in indices[:20]:
+                    data[idx] = 0
+                X = self.tfidf_model.transform([self.add_corpus[j] for j in data.keys()])
+                y = np.fromiter(data.values(), dtype=float)
+                X, y = shuffle(X, y, random_state=0)
+
+                self.clf.fit(X, y)
+                relevance_prob = self.clf.predict_proba(self.tfidf_model.transform(self.add_corpus))[:, 1]
+                k = batch_size + len(labeled_indices)
+                top_k_indices = np.argpartition(relevance_prob, -k)[-k:]
+                sorted_top_k = top_k_indices[np.argsort(relevance_prob[top_k_indices])]
+                top_b_indices = list(set(sorted_top_k) - set(labeled_indices))[-batch_size:]
+                retrieved = [self.inv_added[j].split(' @ ')[1] for j in top_b_indices]
+
+                retrieved_labels = self.check_szz(fix_hash, file, retrieved)
+                for h_i in range(batch_size):
+                    data[top_b_indices[h_i]] = retrieved_labels[h_i]
+                labeled_indices += top_b_indices
+                data = {doc_idx: data[doc_idx] for doc_idx in labeled_indices}
+                batch_size += math.ceil(batch_size / 10)
+                print()
 
 
-def check_szz(hash, file, retrieved):
-    git_repo = GitRepository(os.path.join(BASE_DIR, 'nova'))
-    commit = git_repo.get_commit(hash)
-    last_modified = list(git_repo.get_commits_last_modified_lines(commit)[file])
-    labels = [int(h in last_modified) for h in retrieved]
-    return labels
-
-
-for i, row in train_df.iterrows():
-    fix_hash = row['FixHashId']
-    file = row['File']
-    query = deleted[fix_hash][file]
-    bm25_obj = bm25.BM25(add_corpus)
-    scores = bm25_obj.get_scores(query)
-    best_idx = sorted(range(len(scores)), key=lambda j: scores[j], reverse=True)[0]
-
-    data = dict()
-    data[best_idx] = 1
-    labeled_indices = [best_idx]
-    batch_size = 1
-    while True:
-        indices = np.delete(np.arange(len(add_corpus)), labeled_indices)
-        np.random.shuffle(indices)
-        for idx in indices[:20]:
-            data[idx] = 0
-        X = tfidf_model.transform([add_corpus[j] for j in data.keys()])
-        y = np.fromiter(data.values(), dtype=float)
-        X, y = shuffle(X, y, random_state=0)
-
-        clf = LogisticRegression(random_state=0).fit(X, y)
-        relevance_prob = clf.predict_proba(tfidf_model.transform(add_corpus))[:, 1]
-        k = batch_size + len(labeled_indices)
-        top_k_indices = np.argpartition(relevance_prob, -k)[-k:]
-        sorted_top_k = top_k_indices[np.argsort(relevance_prob[top_k_indices])]
-        top_b_indices = list(set(top_k_indices) - set(labeled_indices))[-batch_size:]
-        retrieved = [inv_added[j].split(' @ ')[1] for j in top_b_indices]
-
-        retrieved_labels = check_szz(fix_hash, file, retrieved)
-        for h_i in range(batch_size):
-            data[top_b_indices[h_i]] = retrieved_labels[h_i]
-        labeled_indices += top_b_indices
-        data = {doc_idx: data[doc_idx] for doc_idx in labeled_indices}
-        batch_size += math.ceil(batch_size / 10)
-        print()
-
-
-# if __name__ == '__main__':
-#     pass
+if __name__ == '__main__':
+    szz = ActiveSZZ(os.path.join(BASE_DIR, 'nova'))
+    szz.extract_features()
+    szz.index_docs()
+    szz.train()
